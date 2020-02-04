ea73b402e855a7b56fb58ac79af165440723e3b0
darshan bordekar <darshanbordekar1819@gmail.com>
2020-02-04 12:25:20 +0530

    Done

--- routes/User/UserProduct.js
+++ routes/User/UserProduct.js
@@ -1,6 +1,5 @@
 let express = require("express");
 let router = express.Router();
-let Joi = require("@hapi/joi");
 let product = require("../../mongodb/Product");
 
 // Fetch Product By Category. [Electronics Clothing]
